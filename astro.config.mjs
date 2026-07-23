e3e4a89ec3b45355de086c406231c514be3267ea
Jason Lee <huacnlee@gmail.com>
2026-07-23 17:10:19 +0800

    fix: use Astro default route handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import { integrations, markdown, vite } from "./src/nimbus/astro-config.ts";
 export default defineConfig({
   site: "https://docs.longportwhale.com",
   srcDir: "./src/nimbus",
-  trailingSlash: "ignore",
   build: {
     format: "file",
   },
-  redirects: {},
   markdown,
   integrations,
   vite,
